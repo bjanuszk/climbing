dbf0b81a0a5468208a4fc508bc0bd5c2d87d87b3
bjanuszkiewicz <bjanuszkiewicz@objectivity.co.uk>
2021-04-13 14:11:59 +0200

    Create terraform module.

--- ops/main.tf
+++ ops/main.tf
@@ -1,13 +1,13 @@
 provider "local" {}
 
 module "file1" {
-  source = "modules/file"
+  source = "./modules/file"
   file_name = "f1.txt"
   file_content = "file 1 content"
 }
 
 module "file2" {
-  source = "modules/file"
+  source = "./modules/file"
   file_name = "f2.txt"
   file_content = "file 2 content"
 }
